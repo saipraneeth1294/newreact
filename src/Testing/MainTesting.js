63518465c7862b80d2e475d2982b3757240638ef
Praneeth <saipraneeth1294@gmail.com>
2020-04-02 10:40:42 +0530

    added a new class component

---
+++ src/Testing/MainTesting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainTesting extends Component {
+    render() {
+        return (
+            <div>
+                Hi this is main testing!!!!
+            </div>
+        );
+    }
+}
+
+export default MainTesting;
